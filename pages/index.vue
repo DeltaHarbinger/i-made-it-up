0144f258df2e96c1f808ad0f1b7343a9ac34d61f
Brandon Chung <deltaharbinger@gmail.com>
2023-09-11 23:19:11 -0500

    I made it up

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup></script>
+
+<template>
+  <main>
+    <section class="h-[50vh] flex items-center justify-center">
+      <h1 class="text-9xl font-bold">I Made It Up!</h1>
+    </section>
+    <section>
+      <article class="text-center space-y-3">
+        <h3 class="text-3xl">Did someone use this link as a source during a discussion or argument?</h3>
+        <p class="text-xl">That means that they absolutely know what they are talking about!</p>
+        <p>
+          Scholarly sources are great, but so is having the confidence to state an opinion based on a surface-level understanding of a topic.
+        </p>
+      </article>
+    </section>
+  </main>
+</template>
+
+<style scoped></style>
